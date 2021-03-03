1d0eb940658955aa674823ecfc54127cd36e964f
Pinembour <pinembour@teppelin.fr>
2021-03-03 12:45:35 +0100

    Add more community patches

--- hooks/aosp_build_pre.sh
+++ hooks/aosp_build_pre.sh
@@ -7,13 +7,16 @@ cd "${AOSP_BUILD_DIR}"
 #patch -p1 --no-backup-if-mismatch < "platform/prebuilts/microg/00002-microg-sigspoof.patch"
 
 # apply community patches
-echo "applying community patch 00001-global-internet-permission-toggle.patch"
+echo "applying community patches"
 community_patches_dir="${ROOT_DIR}/community_patches"
 rm -rf "${community_patches_dir}"
 git clone https://github.com/rattlesnakeos/community_patches "${community_patches_dir}"
 patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00001-global-internet-permission-toggle.patch"
 patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00004-use-cloudflare-dns.patch"
 patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00007-set-seedvault-as-dftl-bkp-provider.patch"
+patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00006-increase-default-maximum-password-length.patch"
+patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00002-round-icon.patch"
+
 
 ## apply custom hosts file
 #custom_hosts_file="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
